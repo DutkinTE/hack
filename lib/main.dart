4e851abfd4660ce0b257007ac7dd07b01871c776
DutkinTE <dutkinte@gmail.com>
2023-09-10 23:55:01 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,8 @@ void main() async {
   await FirebaseApi().initNotification();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
     .then((_) {
-      runApp(const MyApp());
+      runApp(const MyApp()); 
+      
     });
 }
 
